9cfc35ef722957cd3d1da0a2acbe3e6ad4aff964
mjwebb1993 <mjwebb1993@yahoo.com>
2023-12-12 21:57:59 -0600

    INCOMPLETE 6.2

---
+++ components/Footer.js
@@ -0,0 +1,6 @@
+import html from "html-literal";
+() => html`
+  <footer>
+    &copy; 2020 <a href="https://savvycoders.com/">Savvy Coders</a>
+  </footer>
+`;
---
+++ components/Header.js
@@ -0,0 +1,6 @@
+import html from "html-literal";
+() => html`
+  <header>
+    <h1>Example SPA</h1>
+  </header>
+`;
